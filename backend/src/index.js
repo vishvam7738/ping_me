3862188f8c7ed530631290aa8b01a13e4433fda8
vishvam <vishvamaggarwal@gmail.com>
2025-03-20 13:31:00 +0530

    Message routes setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,24 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
-import {connectDB} from "./lib/db.js"
+import { connectDB } from "./lib/db.js"
+
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 const app = express();
 
 dotenv.config()
-const PORT=process.env.PORT
+const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
-    console.log("Server is running on PORT: "+PORT);
+    console.log("Server is running on PORT: " + PORT);
     connectDB()
 });
